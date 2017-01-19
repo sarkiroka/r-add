f771d997dec53958120d3aea897a467d6c2a64e2
sarkiroka <sarki_roka@freemail.hu>
2017-01-19 22:28:38 +0100

    modularization

--- library/pad.js
+++ library/pad.js
@@ -2,6 +2,9 @@
  * pad with zeros
  * @author sarkiroka on 2017.01.13.
  */
-module.exports = function () {
-
+module.exports = function (number, numbers) {
+	while (number.length < numbers) {
+		number = '0' + number;
+	}
+	return number;
 };
