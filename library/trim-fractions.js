3fb321b8b82fb516a484643318c82a8d422405fb
sarkiroka <sarki_roka@freemail.hu>
2017-02-05 22:49:58 +0100

    fractions

--- library/trim-fractions.js
+++ library/trim-fractions.js
@@ -3,10 +3,10 @@
  * @author sarkiroka on 2017.01.29.
  */
 module.exports = function (number) {
-	var retValue=number;
-	if(retValue.indexOf('.')>-1){
-		while(retValue[retValue.length-1]=='0'){
-			retValue=retValue.substr(0,retValue.length-1);
+	var retValue = number;
+	if (retValue.indexOf('.') > -1) {
+		while (retValue[retValue.length - 1] == '0') {
+			retValue = retValue.substr(0, retValue.length - 1);
 		}
 	}
 	return retValue;
