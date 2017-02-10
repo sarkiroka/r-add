11b65e8eba1c308a898f0246c3d46808a517b9ad
sarkiroka <sarki_roka@freemail.hu>
2017-02-10 23:59:29 +0100

    fractions

--- library/add-digts.js
+++ library/add-digts.js
@@ -4,13 +4,13 @@
  */
 var addTable = require('./add-table.json');
 module.exports = function addDigits(a, b, previousCarry) {
-	if(a=='.' && b=='.'){
-		retValue={
-			result:'.',
-			carry:previousCarry
+	if (a == '.' && b == '.') {
+		retValue = {
+			result: '.',
+			carry: previousCarry
 		};
-	}else {
-		var result=1*a+1*b+(previousCarry?1:0);
+	} else {
+		var result = 1 * a + 1 * b + (previousCarry ? 1 : 0);
 		retValue = {
 			result: '' + (result % 10),
 			carry: result > 9
