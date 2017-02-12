8519e44ad7b0cf1a80c70211434c7ccab4c9eb18
sarkiroka <sarki_roka@freemail.hu>
2017-02-13 00:04:38 +0100

    fractions

--- test/big-integer.js
+++ test/big-integer.js
@@ -1,15 +1,15 @@
 var add = require('../.');
-var countOfDigits=1000;
+var countOfDigits = 1000;
 describe('big integers', function () {
 	describe('in positive range', function () {
 		it('works with strings', function (done) {
-			var numberA='';
-			var numberB='';
-			var x='';
-			for(var i=0;i<countOfDigits;i++){
-				numberA+='1';
-				numberB+='1';
-				x+='2';
+			var numberA = '';
+			var numberB = '';
+			var x = '';
+			for (var i = 0; i < countOfDigits; i++) {
+				numberA += '1';
+				numberB += '1';
+				x += '2';
 			}
 			add(numberA, numberB, function (err, result) {
 				expect(err).toEqual(null, 'big integers are valid');
@@ -18,13 +18,13 @@ describe('big integers', function () {
 			});
 		});
 		it('works with carry', function (done) {
-			var numberA='';
-			var numberB='';
-			var x='1';
-			for(var i=0;i<countOfDigits;i++){
-				numberA+='9';
-				numberB+='9';
-				x+=i==countOfDigits-1?'8':'9';
+			var numberA = '';
+			var numberB = '';
+			var x = '1';
+			for (var i = 0; i < countOfDigits; i++) {
+				numberA += '9';
+				numberB += '9';
+				x += i == countOfDigits - 1 ? '8' : '9';
 			}
 			add(numberA, numberB, function (err, result) {
 				expect(err).toEqual(null, 'big integers are valid');
