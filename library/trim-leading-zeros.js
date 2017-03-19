dd2d0ffec2bcb5e7f728f1e33f562be4bf1d32f6
sarkiroka <sarki_roka@freemail.hu>
2017-03-19 22:14:30 +0100

    fractions

--- library/trim-leading-zeros.js
+++ library/trim-leading-zeros.js
@@ -2,8 +2,8 @@
  * trim leading zero
  * @author sarkiroka on 2017.04.08.
  */
-var leadingZeroRegex=/^0+([^.])/;
+var leadingZeroRegex = /^0+([^.])/;
 module.exports = function (number) {
-	var retValue=nember.replace(leadingZeroRegex,'$1');
+	var retValue = nember.replace(leadingZeroRegex, '$1');
 	return retValue;
 };
