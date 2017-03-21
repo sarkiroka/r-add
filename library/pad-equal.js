a25dec9ae32486b0b17cecfaff23f1c69ba07bac
sarkiroka <sarki_roka@freemail.hu>
2017-03-21 23:26:11 +0100

    fractions

--- library/pad-equal.js
+++ library/pad-equal.js
@@ -6,15 +6,23 @@ var padInteger=require('./pad-integer');
 module.exports = function (a, b) {
 	var dotPositionA = a.indexOf('.');
 	var dotPositionB = a.indexOf('.');
-	var numbers,resultA,resultB;
+	var numbers,resultA,resultB,retValue;
 	if(dotPositionA==-1 && dotPositionB==-1){ // all integer
 		numbers=Math.max(a.length, b.length);
 		resultA=padInteger(a, numbers);
 		resultB=padInteger(b, numbers);
+		retValue={
+			length: numbers,
+			a: resultA,
+			b: resultB
+		};
+	} else if(dotPositionA>-1 && dotPositionB>-1){
+		numbers=Math.max(a.length, b.length);
+		retValue={
+			length:numbers,
+			a:a,
+			b:b
+		}
 	}
-	return {
-		length: numbers,
-		a: resultA,
-		b: resultB
-	}
+	return retValue;
 };
