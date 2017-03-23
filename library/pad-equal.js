6d0bcfb7aa2b4110e755fb8fe170f8d559b68e66
sarkiroka <sarki_roka@freemail.hu>
2017-03-23 23:31:48 +0100

    fractions

--- library/pad-equal.js
+++ library/pad-equal.js
@@ -16,12 +16,29 @@ module.exports = function (a, b) {
 			a: resultA,
 			b: resultB
 		};
-	} else if(dotPositionA>-1 && dotPositionB>-1){
+	} else if(dotPositionA>-1 && dotPositionB>-1){ // all float
 		numbers=Math.max(a.length, b.length);
 		retValue={
 			length:numbers,
 			a:a,
 			b:b
+		}
+	}else{
+		if(dotPositionA==-1){
+			a+='.0';
+		}
+		if(dotPositionB){
+			b+='.0';
+		}
+		dotPositionA = a.indexOf('.');
+		dotPositionB = a.indexOf('.');
+		numbers=Math.max(dotPositionA,dotPositionB);
+		resultA=padInteger(a,numbers);
+		resultB=padInteger(b,numbers);
+		retValue={
+			length:numbers,
+			a:resultA,
+			b:resultB
 		}
 	}
 	return retValue;
