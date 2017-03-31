f2597f213cb88efcfc36e443c2805d9ef553b829
sarkiroka <sarki_roka@freemail.hu>
2017-03-31 22:55:02 +0200

    fractions

--- library/pad-equal.js
+++ library/pad-equal.js
@@ -2,40 +2,40 @@
  * pad with zeros
  * @author sarkiroka on 2017.01.13.
  */
-var padInteger=require('./pad-integer');
-var padFloat=require('./pad-float');
+var padInteger = require('./pad-integer');
+var padFloat = require('./pad-float');
 module.exports = function (a, b) {
 	var dotPositionA = a.indexOf('.');
 	var dotPositionB = b.indexOf('.');
-	var numbers,resultA,resultB,retValue;
-	if(dotPositionA==-1 && dotPositionB==-1){ // all integer
-		numbers=Math.max(a.length, b.length);
-		resultA=padInteger(a, numbers);
-		resultB=padInteger(b, numbers);
-		retValue={
+	var numbers, resultA, resultB, retValue;
+	if (dotPositionA == -1 && dotPositionB == -1) { // all integer
+		numbers = Math.max(a.length, b.length);
+		resultA = padInteger(a, numbers);
+		resultB = padInteger(b, numbers);
+		retValue = {
 			length: numbers,
 			a: resultA,
 			b: resultB
 		};
-	}else{ // contain float
-		if(dotPositionA==-1){
-			a+='.0';
+	} else { // contain float
+		if (dotPositionA == -1) {
+			a += '.0';
 		}
-		if(dotPositionB==-1){
-			b+='.0';
+		if (dotPositionB == -1) {
+			b += '.0';
 		}
 		dotPositionA = a.indexOf('.');
 		dotPositionB = b.indexOf('.');
-		numbers=Math.max(dotPositionA,dotPositionB);
-		resultA=padInteger(a,numbers);
-		resultB=padInteger(b,numbers);
-		numbers=Math.max(resultA.length,resultB.length);
-		resultA=padFloat(resultA,numbers);
-		resultB=padFloat(resultB,numbers);
-		retValue={
-			length:numbers,
-			a:resultA,
-			b:resultB
+		numbers = Math.max(dotPositionA, dotPositionB);
+		resultA = padInteger(a, numbers);
+		resultB = padInteger(b, numbers);
+		numbers = Math.max(resultA.length, resultB.length);
+		resultA = padFloat(resultA, numbers);
+		resultB = padFloat(resultB, numbers);
+		retValue = {
+			length: numbers,
+			a: resultA,
+			b: resultB
 		};
 	}
 	return retValue;
