796381073c60d81ad421be0c301ea0c17aadeb9c
sarkiroka <sarki_roka@freemail.hu>
2017-04-01 22:58:26 +0200

    fractions

--- library/pad-integer.js
+++ library/pad-integer.js
@@ -2,8 +2,8 @@
  * pad the integer part
  * @author sarkiroka on 2017.03.18.
  */
-module.exports = function (number, numbers) {
-	while (number.length < numbers) {
+module.exports = function (number, numbers, hasDot) {
+	while ((hasDot?number.indexOf('.'):number.length) < numbers) {
 		number = '0' + number;
 	}
 	return number;
