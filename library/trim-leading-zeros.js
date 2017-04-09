c3480e2c6e2d814dda83b61082a78436f2fe454d
sarkiroka <sarki_roka@freemail.hu>
2017-04-09 22:02:50 +0200

    fractions

--- library/trim-leading-zeros.js
+++ library/trim-leading-zeros.js
@@ -2,6 +2,9 @@
  * trim leading zero
  * @author sarkiroka on 2017.04.08.
  */
-module.exports = function () {
-
+module.exports = function (number) {
+	while(number.indexOf(0)==0){
+		number=number.substr(1);
+	}
+	return number;
 };
