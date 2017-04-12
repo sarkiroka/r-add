018b2a738e574285fffada3c300308bb1308d3c2
sarkiroka <sarki_roka@freemail.hu>
2017-04-12 22:04:25 +0200

    fractions

--- library/normalize-result.js
+++ library/normalize-result.js
@@ -3,9 +3,9 @@
  * @author sarkiroka on 2017.04.04.
  */
 var trimFractions = require('./trim-fractions');
-var trimLeadingZeros=require('./trim-leading-zeros');
+var trimLeadingZeros = require('./trim-leading-zeros');
 module.exports = function (number) {
 	var retValue = trimFractions(number);
-	retValue=trimLeadingZeros(retValue);
+	retValue = trimLeadingZeros(retValue);
 	return retValue;
 };
